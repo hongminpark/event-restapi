849f3b94ff0d10d13a10cccb9ae3fb0a16e6d923
hongmin-park <hongmin.park@navercorp.com>
2020-02-11 23:56:21 +0900

    Add ErrorsSerializer

--- src/main/java/com/navercorp/restapi/events/EventValidator.java
+++ src/main/java/com/navercorp/restapi/events/EventValidator.java
@@ -8,8 +8,12 @@ public class EventValidator {
 
     public void validate(EventDto eventDto, Errors errors) {
         if (eventDto.getBasePrice() > eventDto.getMaxPrice() && eventDto.getMaxPrice() > 0) {
+            // Field Error
             errors.rejectValue("basePrice", "wrongValue", "BasePrice is invalid");
             errors.rejectValue("maxPrice", "wrongValue", "MaxPrice is invalid");
+
+            // Global Error
+            errors.reject("wrongPrices", "Price is invalid");
         }
 
         if (eventDto.getEndEventDateTime().isBefore(eventDto.getBeginEventDateTime()) ||
